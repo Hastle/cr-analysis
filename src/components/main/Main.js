5d17d5cadbf62a80e22c1aa90e923742bd3b9f50
Alexander Egerev <alexandrs.me.com@gmail.com>
2023-06-20 00:57:25 +0300

    create main side component

---
+++ src/components/main/Main.js
@@ -0,0 +1,19 @@
+import React from 'react';
+const Main = () => {
+	return (
+		<main className="col-md-10">
+			<div className="row">
+				<div className="col-md-4">
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate maiores accusantium dolor sapiente, quod, doloremque repellendus et maxime necessitatibus incidunt cumque consectetur similique voluptatum pariatur, omnis adipisci facere quae?</p>
+				</div>
+				<div className="col-md-4">
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate maiores accusantium dolor sapiente, quod, doloremque repellendus et maxime necessitatibus incidunt cumque consectetur similique voluptatum pariatur, omnis adipisci facere quae?</p>
+				</div>
+				<div className="col-md-4">
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate maiores accusantium dolor sapiente, quod, doloremque repellendus et maxime necessitatibus incidunt cumque consectetur similique voluptatum pariatur, omnis adipisci facere quae?</p>
+				</div>
+			</div>
+		</main>
+	);
+}
+export default Main;
