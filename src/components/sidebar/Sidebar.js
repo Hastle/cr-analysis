6d0ab8a05f36f78f3742877fc14213641d51df0e
Alexander Egerev <alexandrs.me.com@gmail.com>
2023-06-20 00:58:17 +0300

    create sidebar component

---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Styles from './sidebar.sass';
+
+const Sidebar = () => {
+	return (
+		<div className="col-md-2 navigation">
+			
+		</div>
+	);
+}
+
+
+export default Sidebar;
