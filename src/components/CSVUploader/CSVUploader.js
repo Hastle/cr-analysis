677314c0da55e75a9738284ddbfb58834601ddfd
Hastle <alexandrs.me.com@gmail.com>
2023-06-23 04:15:12 +0300

    fix empty strings, correct id assignment, add check '0' compliance 'o'

--- src/components/CSVUploader/CSVUploader.js
+++ src/components/CSVUploader/CSVUploader.js
@@ -18,14 +18,41 @@ function CSVUploader({ onDataArr }) {
 	};
 
 	const parseCSV = (data) => {
-		let dataArr = data.split("\n").map(function (line, index) {
-			let point = line.split(",");
-			return {
-				id: index + 1,
-				X: parseInt(point[0]),
-				Y: parseInt(point[1])
-			};
-		});
+		let dataArr = [];
+		let lines = data.split('\n');
+		let id = 1;
+
+		for (let index = 0; index < lines.length; index++) {
+			let line = lines[index].trim();
+			if (line === '') continue; // Пропуск пустых строк
+
+			let point = line.split(',');
+
+			let x = parseFloat(point[0]);
+			let y = parseFloat(point[1]);
+
+			if (isNaN(x) || isNaN(y)) {
+				continue; // Пропуск строки с отсутствующими значениями X или Y
+			}
+
+			// Преобразование символа 'o' в 0, если он не находится в начале значения X
+			if (point[0].indexOf('o') > 0) {
+				x = parseFloat(point[0].replace(/o/g, '0'));
+			}
+
+			// Преобразование символа 'o' в 0, если он не находится в начале значения Y
+			if (point[1].indexOf('o') > 0) {
+				y = parseFloat(point[1].replace(/o/g, '0'));
+			}
+
+			dataArr.push({
+				id: id++,
+				X: x,
+				Y: y,
+			});
+		}
+
+		console.log(dataArr);
 		return dataArr;
 	};
 
