e76e89fc29c98b78cf31e8bd6ea32fcf36e6f6cf
Alexander Egerev <alexandrs.me.com@gmail.com>
2023-06-26 14:55:06 +0300

    use visualization component

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -5,6 +5,7 @@ import IntermediateTable1 from '../tables/IntermediateTable1';
 import RankTable from '../tables/RankTable';
 import IntermediateTable2 from '../tables/IntermediateTable2';
 import Report from '../report/Report';
+import Visualization from '../chart/Visualization';
 
 const Main = () => {
 	const [dataArr, setDataArr] = useState(null);
@@ -17,6 +18,9 @@ const Main = () => {
 		});
 		setDataArr(filteredDataArr);
 	};
+
+	const type = 'straight';
+	const regressionEquation = 'y = 2x + 1';
 
 	return (
 		<main className="col-md-10">
@@ -53,6 +57,11 @@ const Main = () => {
 			<div className="row justify-content-center mt-3">
 				<div className="col-md-8">
 					<Report dataArr={dataArr} />
+				</div>
+			</div>
+			<div className="row justify-content-center mt-3">
+				<div className="col-md-8">
+					<Visualization dataArr={dataArr} type={type} regressionEquation={regressionEquation}/>
 				</div>
 			</div>
 		</main>
