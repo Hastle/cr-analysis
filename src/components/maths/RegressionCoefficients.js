a26954074a76372e95f29a0d014daf02f51f019b
Alexander Egerev <alexandrs.me.com@gmail.com>
2023-09-08 21:15:41 +0300

    correction calculation regression coefficients for regressionType parabola

--- src/components/maths/RegressionCoefficients.js
+++ src/components/maths/RegressionCoefficients.js
@@ -3,7 +3,12 @@ const calculateRegressionCoefficients = (xValues, yValues, regressionType) => {
     let sumX = 0;
     let sumY = 0;
     let sumXY = 0;
+    let sumXXY = 0;
     let sumXX = 0;
+    let sumXXX = 0;
+    let sumXXXX = 0;
+    let sumXlnY = 0;
+    let sumlnY = 0;
 
 
     let a0, a1, a2;
@@ -12,31 +17,49 @@ const calculateRegressionCoefficients = (xValues, yValues, regressionType) => {
         for (let i = 0; i < n; i++) {
             const x = xValues[i];
             const y = yValues[i];
-            const xSquared = x * x;
-
             sumX += x;
             sumY += y;
             sumXY += x * y;
-            sumXX += xSquared;
+            sumXX += x * x;
         }
+
         const determinant = n * sumXX - sumX * sumX;
+
         a1 = (n * sumXY - sumX * sumY) / determinant;
         a0 = (sumY - a1 * sumX) / n;
     } else if (regressionType === 'parabola') {
-        a0 = 0;
-        a1 = 0;
-        a2 = 0;
-    } else if (regressionType === 'exponential') {
-        let sumXlnY = 0;
-        let sumXX = 0;
-        let sumX = 0;
-        let sumlnY = 0;
+        for (let i = 0; i < n; i++) {
+            const x = xValues[i];
+            const y = yValues[i];
+            sumX += x;
+            sumXX += x * x;
+            sumXXX += x * x * x;
+            sumY += y;
+            sumXY += x * y;
+            sumXXY += x * x * y;
+            sumXXXX += x * x * x * x;
+        }
+        sumX = sumX / n;
+        sumXX = sumXX / n;
+        sumXXX = sumXXX / n;
+        sumY = sumY / n;
+        sumXY = sumXY / n;
+        sumXXY = sumXXY / n;
+        sumXXXX = sumXXXX / n;
 
+        const determinant = (sumXX * sumXXXX - sumXXX * sumXXX) - (sumX * (sumX * sumXXXX - sumXX * sumXXX)) + (sumXX * (sumX * sumXXX - sumXX * sumXX));
+        const detA0 = sumY * (sumXX * sumXXXX - sumXXX * sumXXX) - (sumX * (sumXY * sumXXXX - sumXXY * sumXXX)) + (sumXX * (sumXY * sumXXX - sumXXY * sumXX));
+        const detA1 = (sumXY * sumXXXX - sumXXY * sumXXX) - (sumY * (sumX * sumXXXX - sumXX * sumXXX)) + (sumXX * (sumX * sumXXY - sumXX * sumXY));
+        const detA2 = (sumXX * sumXXY - sumXXX * sumXY) - (sumX * (sumX * sumXXY - sumXX * sumXY)) + (sumY * (sumX * sumXXX - sumXX * sumXX));
+
+        a0 = detA0 / determinant;
+        a1 = detA1 / determinant;
+        a2 = detA2 / determinant;
+    } else if (regressionType === 'exponential') {
         for (let i = 0; i < n; i++) {
             const x = xValues[i];
             const y = yValues[i];
             const lnY = Math.log(y);
-
             sumXlnY += x * lnY;
             sumXX += x * x;
             sumX += x;
@@ -59,7 +82,9 @@ const calculateRegressionCoefficients = (xValues, yValues, regressionType) => {
             sumXY += x * y;
             sumXX += x * x;
         }
+
         const determinant = n * sumXX - sumX * sumX;
+
         a1 = (n * sumXY - sumX * sumY) / determinant;
         a0 = (sumY - a1 * sumX) / n;
     } else if (regressionType === 'logarithmic') {
@@ -72,7 +97,9 @@ const calculateRegressionCoefficients = (xValues, yValues, regressionType) => {
             sumXY += x * y;
             sumXX += x * x;
         }
+
         const determinant = n * sumXX - sumX * sumX;
+
         a1 = (n * sumXY - sumX * sumY) / determinant;
         a0 = (sumY - a1 * sumX) / n;
     } else {
