0ece1910a1ce4c0e34e2bd3a57a3f24b4c2e5022
Alexander Egerev <alexandrs.me.com@gmail.com>
2023-09-14 02:17:58 +0300

    correct calculation StandardErrorOfParameters, using regressionType, residualVariance

--- src/components/maths/StandardErrorOfParameters.js
+++ src/components/maths/StandardErrorOfParameters.js
@@ -1,10 +1,30 @@
-const calculateStandardErrorOfParameters = (xValues, yValues, a0, a1) => {
-    const predictedYValues = xValues.map((x) => a0 + a1 * x);
-    const differences = predictedYValues.map((predictedY, index) => predictedY - yValues[index]);
-    const sumSquaredDifferences = differences.reduce((sum, difference) => sum + Math.pow(difference, 2), 0);
+const calculateStandardErrorOfParameters = (xValues, yValues, a0, a1, a2, regressionType, residualVariance) => {
+    let predictedYValues;
+
+    if (regressionType === 'straight') {
+        predictedYValues = xValues.map((x) => a0 + a1 * x);
+    } else if (regressionType === 'parabola') {
+        predictedYValues = xValues.map((x) => a0 + a1 * x + a2 * Math.pow(x, 2));
+    } else if (regressionType === 'exponential') {
+        predictedYValues = xValues.map((x) => a0 * Math.pow(a1, x));
+    } else if (regressionType === 'hyperbola') {
+        predictedYValues = xValues.map((x) => a0 + a1 / x);
+    } else if (regressionType === 'logarithmic') {
+        predictedYValues = xValues.map((x) => a0 + a1 * Math.log(x));
+    }
     const n = xValues.length;
-    const standardErrorA0 = Math.sqrt((sumSquaredDifferences * (1 / n + Math.pow(xValues.reduce((sum, x) => sum + Math.pow(x, 2), 0), 2) / (n * xValues.reduce((sum, x) => sum + x, 0)))) / (n - 2));
-    const standardErrorA1 = Math.sqrt(sumSquaredDifferences / (n * xValues.reduce((sum, x) => sum + Math.pow(x, 2), 0)) / (n - 2));
-    return [standardErrorA0, standardErrorA1];
+    const meanX = xValues.reduce((sum, value) => sum + value, 0) / n;
+    const meanX2 = xValues.reduce((sum, x) => sum + Math.pow(x, 2), 0) / n;
+    const meanX4 = xValues.reduce((sum, x) => sum + Math.pow(x, 4), 0) / n;
+    const sumSquaredDeviations = meanX2 - Math.pow(meanX, 2);
+    const sumSquaredDeviationsX2 = meanX4 - Math.pow(meanX2, 2);
+    const standardErrorA0 = Math.sqrt(residualVariance / (n - 2));
+    const standardErrorA1 = Math.sqrt(residualVariance / (n - 2) / sumSquaredDeviations);
+    let standardErrorA2 = null;
+    if (regressionType === 'parabola') {
+        standardErrorA2 = Math.sqrt(residualVariance / (n - 2) / sumSquaredDeviationsX2);
+    }
+
+    return [standardErrorA0, standardErrorA1, standardErrorA2];
 };
 export default calculateStandardErrorOfParameters;
