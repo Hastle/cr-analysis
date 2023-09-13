66c9cc9e53a1793e632c34fe2c7f279ad52f8206
Alexander Egerev <alexandrs.me.com@gmail.com>
2023-09-14 02:16:00 +0300

    correct calculation residualVariance, using regressionType

--- src/components/maths/ResidualVariance.js
+++ src/components/maths/ResidualVariance.js
@@ -1,6 +1,21 @@
-const calculateResidualVariance = (xValues, a0, a1, yValues) => {
-    const predictedYValues = xValues.map((x) => a0 + a1 * x);
+const calculateResidualVarianceSquared = (xValues, yValues, a0, a1, a2, regressionType) => {
+    let predictedYValues;
+
+    if (regressionType === 'straight') {
+        predictedYValues = xValues.map((x) => a0 + a1 * x);
+    } else if (regressionType === 'parabola') {
+        predictedYValues = xValues.map((x) => a0 + a1 * x + a2 * Math.pow(x, 2));
+    } else if (regressionType === 'exponential') {
+        predictedYValues = xValues.map((x) => a0 * Math.pow(a1, x));
+    } else if (regressionType === 'hyperbola') {
+        predictedYValues = xValues.map((x) => a0 + a1 / x);
+    } else if (regressionType === 'logarithmic') {
+        predictedYValues = xValues.map((x) => a0 + a1 * Math.log(x));
+    }
+    const n = yValues.length;
+
     const squaredDeviations = yValues.map((value, index) => Math.pow(value - predictedYValues[index], 2));
-    return squaredDeviations.reduce((sum, deviation) => sum + deviation, 0);;
+
+    return squaredDeviations.reduce((sum, deviation) => sum + deviation, 0) / n;
 };
-export default calculateResidualVariance;
+export default calculateResidualVarianceSquared;
