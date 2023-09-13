c125ea7dcfbf0900ba1f34e187ba2a1a1e2abebe
Alexander Egerev <alexandrs.me.com@gmail.com>
2023-09-14 02:25:30 +0300

    correct calculation meanApproximationError, using a2, regressionType

--- src/components/maths/MeanApproximationError.js
+++ src/components/maths/MeanApproximationError.js
@@ -1,7 +1,22 @@
-const calculateMeanApproximationError = (xValues, yValues, a0, a1) => {
-    const predictedYValues = xValues.map((x) => a0 + a1 * x);
-    const squaredDifferences = predictedYValues.map((predictedY, index) => Math.pow(predictedY - yValues[index], 2));
-    const sumSquaredDifferences = squaredDifferences.reduce((sum, difference) => sum + difference, 0);
-    return Math.sqrt(sumSquaredDifferences / predictedYValues.length);;
+const calculateMeanApproximationError = (xValues, yValues, a0, a1, a2, regressionType) => {
+    let predictedYValues;
+
+    if (regressionType === 'straight') {
+        predictedYValues = xValues.map((x) => a0 + a1 * x);
+    } else if (regressionType === 'parabola') {
+        predictedYValues = xValues.map((x) => a0 + a1 * x + a2 * Math.pow(x, 2));
+    } else if (regressionType === 'exponential') {
+        predictedYValues = xValues.map((x) => a0 * Math.pow(a1, x));
+    } else if (regressionType === 'hyperbola') {
+        predictedYValues = xValues.map((x) => a0 + a1 / x);
+    } else if (regressionType === 'logarithmic') {
+        predictedYValues = xValues.map((x) => a0 + a1 * Math.log(x));
+    }
+
+    const absoluteDifferences = predictedYValues.map((predictedY, index) => Math.abs(yValues[index] - predictedY));
+    const sumAbsoluteDifferences = absoluteDifferences.reduce((sum, difference) => sum + difference, 0);
+    const sumYValues = yValues.reduce((sum, value) => sum + value, 0);
+
+    return sumAbsoluteDifferences / sumYValues;
 };
-export default calculateMeanApproximationError
+export default calculateMeanApproximationError;
