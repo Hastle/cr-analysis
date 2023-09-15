b18d74ddd8b0eda504b6a1c3c9c4bf4fa464861a
Alexander Egerev <alexandrs.me.com@gmail.com>
2023-09-15 22:40:17 +0300

    correct calculation fisherCriterion, using regressionType

--- src/components/maths/FisherCriterion.js
+++ src/components/maths/FisherCriterion.js
@@ -1,4 +1,9 @@
-const calculateFisherCriterion = (factorVariance, residualVariance, k, n) => {
-    return (factorVariance / k) / (residualVariance / (n - k - 1));
+const calculateFisherCriterion = (factorVariance, residualVariance, regressionType, n) => {
+    let k;
+    if (regressionType === 'parabola')
+        k = 3;
+    else
+        k = 2;
+    return (factorVariance / (k - 1)) / (residualVariance / (n - k));
 };
 export default calculateFisherCriterion;
