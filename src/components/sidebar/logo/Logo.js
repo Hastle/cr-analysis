59f1d40b58a53c8f01af539f5d2ba657c3936e2a
Alexander Egerev <alexandrs.me.com@gmail.com>
2023-09-26 03:43:31 +0300

    Revert "use the Logo, create buttons to add dataArrExample to DataContext"
    This reverts commit 06b0a0faddc660f2fd36ab5b008624bdfd434f03.

--- src/components/sidebar/logo/Logo.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './logo.sass';
-import Logotype from './CR.png';
-
-export default function Logo () {
-    return (
-        <a href="/" className="logo mt-3"></a>
-    )
-}
