5aee9bf694328572d845c3c28f6da40c21be6b91
Alexander Egerev <alexandrs.me.com@gmail.com>
2023-09-26 04:06:31 +0300

    create Logo

---
+++ src/components/sidebar/logo/Logo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './logo.sass';
+import Logotype from './CR.png';
+
+export default function Logo () {
+    return (
+        <a href="/" className="logo mt-3"></a>
+    )
+}
